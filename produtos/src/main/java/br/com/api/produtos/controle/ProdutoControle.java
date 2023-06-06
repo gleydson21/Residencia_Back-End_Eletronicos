c522bd4565b6a77e7de3fec984204979f8b9622d
Gleydson dias Ferreira <111371885+gleydson21@users.noreply.github.com>
2023-06-06 03:13:45 -0300

    início do Back End do Projeto com banco de dados
    banco de dados utilizado: MYSQL

---
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -0,0 +1,13 @@
+package br.com.api.produtos.controle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProdutoControle {
+    
+    @GetMapping
+    public String rota(){
+        return "API de Produtos Funcionando!";
+    }
+}
